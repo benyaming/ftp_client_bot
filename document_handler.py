33a4b524dc3e379bc6c1d8df52d9ce8d60f71366
Benyomin Ginzburg <benyomin.94@gmail.com>
2019-04-08 12:14:23 +0300

    Testing generic media class

--- document_handler.py
+++ document_handler.py
@@ -5,36 +5,81 @@ from telebot import TeleBot
 import db
 
 
-class PhotoHandler(object):
+# class PhotoHandler(object):
+#
+#     def __init__(self, user_id: int, link: str, caption: str):
+#         self._user_id = user_id
+#         self._link = link
+#         self._client_name = db.get_client_name(self._user_id)
+#         self._caption = caption
+#
+#     def handle_photo(self):
+#         self._forward_photo_to_admin()
+#
+#     def _forward_photo_to_admin(self):
+#         token = db.get_client_bot_token(self._user_id)
+#         admin_bot = TeleBot(token)
+#         operators = db.get_operators(self._user_id)
+#         if self._caption:
+#             caption = f'<b>{self._client_name}</b>\n\n{self._caption}'
+#         else:
+#             caption = f'<b>{self._client_name}</b>'
+#
+#         connection_pool = urllib3.PoolManager()
+#         resp = connection_pool.request('GET', self._link)
+#
+#         for operator in operators:
+#             admin_bot.send_photo(operator, resp.data, caption,
+#                                  parse_mode='HTML')
+#         resp.release_conn()
+#
+#
+# class DocumentHandler(object):
+#
+#     def __init__(self, user_id: int, link: str, caption: str = None):
+#         self._user_id = user_id
+#         self._link = link
+#         self._client_name = db.get_client_name(self._user_id)
+#         self._caption = caption
+#
+#     def handle_document(self):
+#         self._forward_document_to_admin()
+#
+#     def handle_voice(self):
+#         self._forward_voice_to_admin()
+#
+#     def _forward_document_to_admin(self):
+#         token = db.get_client_bot_token(self._user_id)
+#         admin_bot = TeleBot(token)
+#         operators = db.get_operators(self._user_id)
+#         if self._caption:
+#             caption = f'<b>{self._client_name}</b>\n\n{self._caption}'
+#         else:
+#             caption = f'<b>{self._client_name}</b>'
+#
+#         connection_pool = urllib3.PoolManager()
+#         resp = connection_pool.request('GET', self._link)
+#
+#         for operator in operators:
+#             admin_bot.send_document(operator, resp.data, caption=caption,
+#                                     parse_mode='HTML')
+#         resp.release_conn()
+#
+#     def _forward_voice_to_admin(self):
+#         token = db.get_client_bot_token(self._user_id)
+#         admin_bot = TeleBot(token)
+#         operators = db.get_operators(self._user_id)
+#         caption = f'<b>{self._client_name}</b>'
+#
+#         connection_pool = urllib3.PoolManager()
+#         resp = connection_pool.request('GET', self._link)
+#
+#         for operator in operators:
+#             admin_bot.send_voice(operator, resp.data, caption, parse_mode='HTML',)
+#         resp.release_conn()
 
-    def __init__(self, user_id: int, link: str, caption: str):
-        self._user_id = user_id
-        self._link = link
-        self._client_name = db.get_client_name(self._user_id)
-        self._caption = caption
 
-    def handle_photo(self):
-        self._forward_photo_to_admin()
-
-    def _forward_photo_to_admin(self):
-        token = db.get_client_bot_token(self._user_id)
-        admin_bot = TeleBot(token)
-        operators = db.get_operators(self._user_id)
-        if self._caption:
-            caption = f'<b>{self._client_name}</b>\n\n{self._caption}'
-        else:
-            caption = f'<b>{self._client_name}</b>'
-
-        connection_pool = urllib3.PoolManager()
-        resp = connection_pool.request('GET', self._link)
-
-        for operator in operators:
-            admin_bot.send_photo(operator, resp.data, caption,
-                                 parse_mode='HTML')
-        resp.release_conn()
-
-
-class DocumentHandler(object):
+class MediaHandler(object):
 
     def __init__(self, user_id: int, link: str, caption: str = None):
         self._user_id = user_id
@@ -42,13 +87,7 @@ class DocumentHandler(object):
         self._client_name = db.get_client_name(self._user_id)
         self._caption = caption
 
-    def handle_document(self):
-        self._forward_document_to_admin()
-
-    def handle_voice(self):
-        self._forward_voice_to_admin()
-
-    def _forward_document_to_admin(self):
+    def handle_media(self, media_type: str = 'document'):
         token = db.get_client_bot_token(self._user_id)
         admin_bot = TeleBot(token)
         operators = db.get_operators(self._user_id)
@@ -58,22 +97,16 @@ class DocumentHandler(object):
             caption = f'<b>{self._client_name}</b>'
 
         connection_pool = urllib3.PoolManager()
-        resp = connection_pool.request('GET', self._link)
+        media = connection_pool.request('GET', self._link).data
+
+        actions = {
+            'docunemt': admin_bot.send_document,
+            'photo': admin_bot.send_photo,
+            'voice': admin_bot.send_voice
+        }
+        send_media = actions.get(media_type)
 
         for operator in operators:
-            admin_bot.send_document(operator, resp.data, caption=caption,
-                                    parse_mode='HTML')
-        resp.release_conn()
-
-    def _forward_voice_to_admin(self):
-        token = db.get_client_bot_token(self._user_id)
-        admin_bot = TeleBot(token)
-        operators = db.get_operators(self._user_id)
-        caption = f'<b>{self._client_name}</b>'
-
-        connection_pool = urllib3.PoolManager()
-        resp = connection_pool.request('GET', self._link)
-
-        for operator in operators:
-            admin_bot.send_voice(operator, resp.data, caption, parse_mode='HTML',)
-        resp.release_conn()
+            send_media(operator, )
+            admin_bot.send_document(operator, media, caption=caption, parse_mode='HTML')
+        media.release_conn()
